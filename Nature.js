7f684f5e62605e569637368b4b225dcf7de1bd87
Alyaraj Srivastava <69211436+Alyaraj-frontwebdev@users.noreply.github.com>
2022-09-02 14:58:49 +0530

    Add files via upload

---
+++ Nature.js
@@ -0,0 +1,60 @@
+let select = document.getElementsByClassName('link');
+let video = document.querySelector('.bg-video');
+let h1 = document.querySelector('h1');
+let button = document.querySelector('button');
+let icon = document.querySelector('.icon');
+
+for(let i=0;i<select.length;i++){
+    select[i].addEventListener('click', ()=>{
+        if(select[i].innerText == "SUNRISE"){
+            video.innerHTML = ` <video autoplay loop muted plays-inline class="video">
+            <source src="C:/Users/alyap/Documents/Web/Project/Videos/pexels-taryn-elliott-8827862.mp4">
+           </video>`;
+           h1.innerText = "Sunrise";
+           button.innerText = "About Sunrise →";
+           icon.innerHTML = ` <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="currentColor" class="bi bi-sunrise-fill" viewBox="0 0 16 16">
+           <path d="M7.646 1.146a.5.5 0 0 1 .708 0l1.5 1.5a.5.5 0 0 1-.708.708L8.5 2.707V4.5a.5.5 0 0 1-1 0V2.707l-.646.647a.5.5 0 1 1-.708-.708l1.5-1.5zM2.343 4.343a.5.5 0 0 1 .707 0l1.414 1.414a.5.5 0 0 1-.707.707L2.343 5.05a.5.5 0 0 1 0-.707zm11.314 0a.5.5 0 0 1 0 .707l-1.414 1.414a.5.5 0 1 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zM11.709 11.5a4 4 0 1 0-7.418 0H.5a.5.5 0 0 0 0 1h15a.5.5 0 0 0 0-1h-3.79zM0 10a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2A.5.5 0 0 1 0 10zm13 0a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5z"/>
+         </svg>`;
+        }
+        else if(select[i].innerText == "RAIN"){
+            video.innerHTML = ` <video autoplay loop muted plays-inline class="video">
+            <source src="C:/Users/alyap/Documents/Web/Project/Videos/Pexels Videos 4618.mp4">
+           </video>`;
+           h1.innerText = "Rain";
+           button.innerText = "About Rain →";
+           icon.innerHTML = ` <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="currentColor" class="bi bi-droplet-fill" viewBox="0 0 16 16">
+           <path d="M8 16a6 6 0 0 0 6-6c0-1.655-1.122-2.904-2.432-4.362C10.254 4.176 8.75 2.503 8 0c0 0-6 5.686-6 10a6 6 0 0 0 6 6ZM6.646 4.646l.708.708c-.29.29-1.128 1.311-1.907 2.87l-.894-.448c.82-1.641 1.717-2.753 2.093-3.13Z"/>
+         </svg>`;
+        }
+        else if(select[i].innerText == "FOREST"){
+            video.innerHTML = ` <video autoplay loop muted plays-inline class="video">
+            <source src="C:/Users/alyap/Documents/Web/Project/Videos/Pexels Videos 1448735.mp4">
+           </video>`;
+           h1.innerText = "Forest";
+           button.innerText = "About Forest →";
+           icon.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="currentColor" class="bi bi-tree-fill" viewBox="0 0 16 16">
+           <path d="M8.416.223a.5.5 0 0 0-.832 0l-3 4.5A.5.5 0 0 0 5 5.5h.098L3.076 8.735A.5.5 0 0 0 3.5 9.5h.191l-1.638 3.276a.5.5 0 0 0 .447.724H7V16h2v-2.5h4.5a.5.5 0 0 0 .447-.724L12.31 9.5h.191a.5.5 0 0 0 .424-.765L10.902 5.5H11a.5.5 0 0 0 .416-.777l-3-4.5z"/>
+         </svg> `;
+        }
+        else if(select[i].innerText == "CLOUDS"){
+            video.innerHTML = ` <video autoplay loop muted plays-inline class="video">
+            <source src="C:/Users/alyap/Documents/Web/Project/Videos/production ID_4288017.mp4">
+           </video>`;
+           h1.innerText = "Clouds";
+           button.innerText = "About Clouds →";
+           icon.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="currentColor" class="bi bi-cloud-fill" viewBox="0 0 16 16">
+           <path d="M4.406 3.342A5.53 5.53 0 0 1 8 2c2.69 0 4.923 2 5.166 4.579C14.758 6.804 16 8.137 16 9.773 16 11.569 14.502 13 12.687 13H3.781C1.708 13 0 11.366 0 9.318c0-1.763 1.266-3.223 2.942-3.593.143-.863.698-1.723 1.464-2.383z"/>
+         </svg> `;
+        }
+        else{
+            video.innerHTML = ` <video autoplay loop muted plays-inline class="video">
+            <source src="C:/Users/alyap/Documents/Web/Project/Videos/pexels-rostislav-uzunov-7513671.mp4">
+           </video>`;
+           h1.innerText = "Sea";
+           button.innerText = "About Sea →";
+           icon.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="currentColor" class="bi bi-water" viewBox="0 0 16 16">
+           <path d="M.036 3.314a.5.5 0 0 1 .65-.278l1.757.703a1.5 1.5 0 0 0 1.114 0l1.014-.406a2.5 2.5 0 0 1 1.857 0l1.015.406a1.5 1.5 0 0 0 1.114 0l1.014-.406a2.5 2.5 0 0 1 1.857 0l1.015.406a1.5 1.5 0 0 0 1.114 0l1.757-.703a.5.5 0 1 1 .372.928l-1.758.703a2.5 2.5 0 0 1-1.857 0l-1.014-.406a1.5 1.5 0 0 0-1.114 0l-1.015.406a2.5 2.5 0 0 1-1.857 0l-1.014-.406a1.5 1.5 0 0 0-1.114 0l-1.015.406a2.5 2.5 0 0 1-1.857 0L.314 3.964a.5.5 0 0 1-.278-.65zm0 3a.5.5 0 0 1 .65-.278l1.757.703a1.5 1.5 0 0 0 1.114 0l1.014-.406a2.5 2.5 0 0 1 1.857 0l1.015.406a1.5 1.5 0 0 0 1.114 0l1.014-.406a2.5 2.5 0 0 1 1.857 0l1.015.406a1.5 1.5 0 0 0 1.114 0l1.757-.703a.5.5 0 1 1 .372.928l-1.758.703a2.5 2.5 0 0 1-1.857 0l-1.014-.406a1.5 1.5 0 0 0-1.114 0l-1.015.406a2.5 2.5 0 0 1-1.857 0l-1.014-.406a1.5 1.5 0 0 0-1.114 0l-1.015.406a2.5 2.5 0 0 1-1.857 0L.314 6.964a.5.5 0 0 1-.278-.65zm0 3a.5.5 0 0 1 .65-.278l1.757.703a1.5 1.5 0 0 0 1.114 0l1.014-.406a2.5 2.5 0 0 1 1.857 0l1.015.406a1.5 1.5 0 0 0 1.114 0l1.014-.406a2.5 2.5 0 0 1 1.857 0l1.015.406a1.5 1.5 0 0 0 1.114 0l1.757-.703a.5.5 0 1 1 .372.928l-1.758.703a2.5 2.5 0 0 1-1.857 0l-1.014-.406a1.5 1.5 0 0 0-1.114 0l-1.015.406a2.5 2.5 0 0 1-1.857 0l-1.014-.406a1.5 1.5 0 0 0-1.114 0l-1.015.406a2.5 2.5 0 0 1-1.857 0L.314 9.964a.5.5 0 0 1-.278-.65zm0 3a.5.5 0 0 1 .65-.278l1.757.703a1.5 1.5 0 0 0 1.114 0l1.014-.406a2.5 2.5 0 0 1 1.857 0l1.015.406a1.5 1.5 0 0 0 1.114 0l1.014-.406a2.5 2.5 0 0 1 1.857 0l1.015.406a1.5 1.5 0 0 0 1.114 0l1.757-.703a.5.5 0 1 1 .372.928l-1.758.703a2.5 2.5 0 0 1-1.857 0l-1.014-.406a1.5 1.5 0 0 0-1.114 0l-1.015.406a2.5 2.5 0 0 1-1.857 0l-1.014-.406a1.5 1.5 0 0 0-1.114 0l-1.015.406a2.5 2.5 0 0 1-1.857 0l-1.757-.703a.5.5 0 0 1-.278-.65z"/>
+         </svg> `;
+        }
+    });
+}
